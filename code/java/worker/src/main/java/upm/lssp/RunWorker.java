ef2043ce8c7ae22549da302cbf249894e2ab28d1
Filippo Calzavara <filippo.calzavara@mail.polimi.it>
2018-11-22 13:51:14 +0100

    Better UI

--- code/java/worker/src/main/java/upm/lssp/RunWorker.java
+++ code/java/worker/src/main/java/upm/lssp/RunWorker.java
@@ -1,29 +1,22 @@
 package upm.lssp;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import upm.lssp.worker.ZookeeperWorker;
 
 
 
 public class RunWorker extends Application implements Runnable {
 
     public void start(Stage stage) throws Exception {
-       /* ZookeeperWorker zooWorker = new ZookeeperWorker();
-        RunWorker rw = new RunWorker();
-        rw.run();*/
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
 
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         stage.setTitle("WhatsBaap");
         stage.setScene(new Scene(root));
         stage.show();
+        if(Config.DEBUG) System.out.println("UI Loaded");
 
 
     }
@@ -32,9 +25,8 @@ public class RunWorker extends Application implements Runnable {
     private final Object lock = new Lock();
 
     public static void main(String[] args) {
-
         launch(args);
-
+        new RunWorker().run();
     }
 
 
--- code/java/worker/src/main/java/upm/lssp/ui/LoginController.java
+++ code/java/worker/src/main/java/upm/lssp/ui/LoginController.java
@@ -1,11 +1,13 @@
 package upm.lssp.ui;
 
-import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+import upm.lssp.Config;
 
 public class LoginController {
-public Button login;
-    public void login(){
+public TextField username;
 
-        login.setText("Hello");
+    public void login(){
+        if(Config.DEBUG) System.out.println("Login request for: "+username.getText());
     }
 }
